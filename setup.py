5b214096cc0a6b0836c71cfd80ab8699f337f057
David Gossow <david@hidof.com>
2014-04-16 15:01:41 -0700

    Imported upstream version '1.9.10' of 'upstream'

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['interactive_markers'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    requires=['rospy', 'visualization_msgs', 'std_msgs', 'roslib']
 )
 
 setup(**d)
